07ffd28392b516fc683a29b586cf3dd47f103a90
abdalla3yash <abdo3yash@gmail.com>
2023-09-22 19:24:50 +0300

    add BaseUrl and pallete color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/spotify.dart';
+import 'utils/utils.dart';
 
-void main() {
+void main() async {
+  var spotify = SpotifyApi(credentials);
+
+  print('\nAlbum:');
+  var album = await spotify.albums.get('2Hog1V8mdTWKhCYqI5paph');
+  print(album.name);
+
+  print('\nAlbum Tracks:');
+  var tracks = await spotify.albums.getTracks(album.id!).all();
+  tracks.forEach((track) {
+    print(track.name);
+  });
+
+  print('\nNew Releases');
+  var newReleases = await spotify.browse.getNewReleases().first();
+  newReleases.items!.forEach((album) => print(album.name));
+
+  print('\nFeatured Playlist:');
+  var featuredPlaylists = await spotify.playlists.featured.all();
+  featuredPlaylists.forEach((playlist) {
+    print(playlist.name);
+  });
+
+  print("\nSearching for \'Metallica\':");
+  var search = await spotify.search.get('metallica').first(2);
+
+  search.forEach((pages) {
+    if (pages.items == null) {
+      print('Empty items');
+    }
+    pages.items!.forEach((item) {
+      if (item is PlaylistSimple) {
+        print('Playlist: \n'
+            'id: ${item.id}\n'
+            'name: ${item.name}:\n'
+            'collaborative: ${item.collaborative}\n'
+            'href: ${item.href}\n'
+            'trackslink: ${item.tracksLink!.href}\n'
+            'owner: ${item.owner}\n'
+            'public: ${item.owner}\n'
+            'snapshotId: ${item.snapshotId}\n'
+            'type: ${item.type}\n'
+            'uri: ${item.uri}\n'
+            'images: ${item.images!.length}\n'
+            '-------------------------------');
+      }
+      if (item is Artist) {
+        print('Artist: \n'
+            'id: ${item.id}\n'
+            'name: ${item.name}\n'
+            'href: ${item.href}\n'
+            'type: ${item.type}\n'
+            'uri: ${item.uri}\n'
+            'popularity: ${item.popularity}\n'
+            '-------------------------------');
+      }
+      if (item is Track) {
+        print('Track:\n'
+            'id: ${item.id}\n'
+            'name: ${item.name}\n'
+            'href: ${item.href}\n'
+            'type: ${item.type}\n'
+            'uri: ${item.uri}\n'
+            'isPlayable: ${item.isPlayable}\n'
+            'artists: ${item.artists!.length}\n'
+            'availableMarkets: ${item.availableMarkets!.length}\n'
+            'discNumber: ${item.discNumber}\n'
+            'trackNumber: ${item.trackNumber}\n'
+            'explicit: ${item.explicit}\n'
+            'popularity: ${item.popularity}\n'
+            '-------------------------------');
+      }
+      if (item is AlbumSimple) {
+        print('Album:\n'
+            'id: ${item.id}\n'
+            'name: ${item.name}\n'
+            'href: ${item.href}\n'
+            'type: ${item.type}\n'
+            'uri: ${item.uri}\n'
+            'albumType: ${item.albumType}\n'
+            'artists: ${item.artists!.length}\n'
+            'availableMarkets: ${item.availableMarkets!.length}\n'
+            'images: ${item.images!.length}\n'
+            'releaseDate: ${item.releaseDate}\n'
+            'releaseDatePrecision: ${item.releaseDatePrecision}\n'
+            '-------------------------------');
+      }
+    });
+  });
+
+  var relatedArtists =
+      await spotify.artists.relatedArtists('0OdUWJ0sBjDrqHygGUXeCF');
+  print('\nRelated Artists: ${relatedArtists.length}');
+
+  credentials = await spotify.getCredentials();
+  print('\nCredentials:');
+  print('Client Id: ${credentials.clientId}');
+  print('Access Token: ${credentials.accessToken}');
+  print('Credentials Expired: ${credentials.isExpired}');
   runApp(const MyApp());
 }
 
@@ -13,21 +113,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,15 +123,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,50 +135,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
